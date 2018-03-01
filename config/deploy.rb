517e07c30ccf9cfa4ad5560e073c685024be9cec
Max <max.bianchi@hotmail.fr>
2018-03-01 16:28:41 +0100

    please;

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:Moutoneige/newsite.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "home508971167.1and1-data.host/dev_test"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
